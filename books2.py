c05b3741becee9322ed63524a08ac951f4482728
mahdighorbani98 <mahdig502@gmail.com>
2025-11-04 16:19:34 +0330

    refactor: add BaseModel and BookRequest for book_request

--- books2.py
+++ books2.py
@@ -4,16 +4,16 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class BOOK:
+class Book:
     id: int
     title: str
     author: str
     description: str
     rating: int
 
-    def __init__(self, id, titel, author, description, rating):
+    def __init__(self, id, title, author, description, rating):
         self.id = id
-        self.titel = titel
+        self.title = title
         self.author = author
         self.description = description
         self.rating = rating
@@ -28,9 +28,9 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    BOOK(1, 'B1', 'Ali', 'A nice book!', 5),
-    BOOK(2, 'B2', 'Ehsan', 'A very nice book!', 4),
-    BOOK(3, 'B3', 'Ali', 'A normal book!', 3)
+    Book(1, 'B1', 'Ali', 'A nice book!', 5),
+    Book(2, 'B2', 'Ehsan', 'A very nice book!', 4),
+    Book(3, 'B3', 'Ali', 'A normal book!', 3)
 ]
 
 
@@ -40,5 +40,7 @@ async def read_all_books():
 
 
 @app.post('/create_book')
-async def create_book(new_book: BookRequest):
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    print(type(new_book))
     BOOKS.append(new_book)
