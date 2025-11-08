8062ab20bc0b59ceca6aba945b7df2b4e7f08d70
mahdighorbani98 <mahdig502@gmail.com>
2025-11-08 19:58:18 +0330

    feat: add Path to validate dynamic path values

--- books2.py
+++ books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Path
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -65,7 +65,7 @@ async def read_book():
 
 
 @app.get('/books/{book_id}')
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -109,7 +109,7 @@ async def update_book(book: BookRequest):
 
 
 @app.delete('/books/{book_id}')
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
