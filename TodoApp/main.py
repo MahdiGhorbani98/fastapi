7df09d50178a420246e90bb27479c07c0f916431
mahdighorbani98 <mahdig502@gmail.com>
2025-11-14 21:20:24 +0330

    feat: post endpoint in Todos

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,6 +1,7 @@
 # Import FastAPI to create the web application
 from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, Path
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -28,6 +29,24 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=1)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Task title",
+                "description": "Task description",
+                "priority": 5,
+                "complete": False
+            }
+        }
+    }
+
+
 @app.get('/', status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
@@ -39,3 +58,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post('/todo', status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
