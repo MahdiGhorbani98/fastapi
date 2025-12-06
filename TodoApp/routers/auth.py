5085f789e8ab1f4496d2b8c0377fce737101fcf4
mahdighorbani98 <mahdig502@gmail.com>
2025-12-06 17:10:20 +0330

    feat: decode jwt for get_current_user in all endpoints

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,14 +1,15 @@
 from datetime import timedelta, timezone, datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
-from jose import jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+from starlette import status
 
 SECRET_KEY = '9cf70cf99954d4d0b7f558199aafc9a50300f834f7e1cc3f801d467bae672c1d'
 ALGORITHM = 'HS256'
@@ -16,6 +17,7 @@ ALGORITHM = 'HS256'
 router = APIRouter()
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 class CreateUserRequest(BaseModel):
@@ -57,6 +59,20 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {'username': username, 'id': user_id}
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
 
 @router.post('/auth', status_code=status.HTTP_201_CREATED)
